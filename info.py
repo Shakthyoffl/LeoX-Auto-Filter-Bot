469a3ccb1c826e4504f186eb3e664b13e889cfe8
Shakthyoffl <160217199+Shakthyoffl@users.noreply.github.com>
2025-12-07 23:56:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'LeoX 1TamilMovie.com')
 API_ID = int(environ.get('API_ID', '27549502'))
 API_HASH = environ.get('API_HASH', '35892287036e27af8e9298f03483fdd4')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7735629958:AAHS6SJWRoVxvCgmiADn6lswZ3c4PMpCfxQ")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8194517058:AAHJ_5P1ravR1m782aoZDMbb9O3Qv3odqO8")
 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
